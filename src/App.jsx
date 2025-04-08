6bea0efc9c447f8be27c131388c34503bfe3dc5a
RickyBv1 <rickycortes668668@gmail.com>
2025-04-07 20:52:09 -0400

    Refactor code structure for improved readability and maintainability

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
+import { Products } from './components/Products'
+import { products } from './mocks/products'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      
+      <Products products={products} />
     </>
   )
 }

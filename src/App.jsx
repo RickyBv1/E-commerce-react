7678c60f994e2c05c5a9cd063ee527459cb97d84
RickyBv1 <rickycortes668668@gmail.com>
2025-04-09 18:31:28 -0400

    feat: refactor Filters and Header components; implement useFilters hook for better state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,20 @@
-import { useContext, useState } from "react";
+import { useState } from "react";
 import { Header } from "./components/Header";
 import { Products } from "./components/Products";
 import { Footer } from "./components/Footer";
 import { products as initialProducts } from "./mocks/products";
+import { useFilters } from "./hooks/useFilters";
 import "./App.css";
-import { FiltersContext } from "./context/filters";
-
-function useFilters() {
-  // const [filters, setFilters] = useState({
-  //   category: "all",
-  //   minPrice: 0,
-  // });
-  const { filters, setFilters } = useContext(FiltersContext);
-
-  const filterProducts = (products) => {
-    return products.filter((product) => {
-      return (
-        product.price >= filters.minPrice &&
-        (filters.category === "all" || product.category === filters.category)
-      );
-    });
-  };
-
-  return { filterProducts, setFilters };
-}
 
 function App() {
   const [products] = useState(initialProducts);
-  const { filterProducts, setFilters } = useFilters();
+  const { filterProducts } = useFilters();
 
   const filteredProducts = filterProducts(products);
 
   return (
     <>
-      <Header changeFilters={setFilters} />
+      <Header />
       <Products products={filteredProducts} />
       <Footer />
     </>
